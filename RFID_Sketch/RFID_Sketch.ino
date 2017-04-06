7e70871b33fc6401ac7316998b99adef9ec65c4a
XylofonKobra414 <xylofonkobra414@gmail.com>
2017-04-06 15:39:26 +0200

    RFID-Quellcode
    Der Quellcode des RFID Projektes

---
+++ RFID_Sketch/RFID_Sketch.ino
@@ -0,0 +1,22 @@
+/* 
+  RFID - Chip (Schreiben und lesen)
+
+  (c)2017 Louis Janosch, Oliver Wawer, Mike Kamps
+*/
+
+#define SDA_Pin 10
+#define SCK_Pin 13
+#define MOSI_Pin 11
+#define MISO_Pin 12
+#define RST_Pin 5
+
+void setup()
+{
+  pinMode(SDA_Pin, OUTPUT);
+}
+
+void loop()
+{
+  
+}
+
